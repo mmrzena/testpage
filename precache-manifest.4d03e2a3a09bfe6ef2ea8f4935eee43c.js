7318dda8202cbbd287e18d2a34c82115c8c31179
Michal Mrzena <michal.mrzena@gmail.com>
2018-10-19 13:11:17 +0200

    Updates

---
+++ precache-manifest.4d03e2a3a09bfe6ef2ea8f4935eee43c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f267452189c2c91aad853a9e5fcc8cc3",
+    "url": "/testpage/static/media/thumbnail3.f2674521.png"
+  },
+  {
+    "revision": "ad8401390a6abb73daae",
+    "url": "/testpage/static/css/main.937c2814.chunk.css"
+  },
+  {
+    "revision": "ab8e1271375bc5416fc8",
+    "url": "/testpage/static/js/1.ab8e1271.chunk.js"
+  },
+  {
+    "revision": "e35f2ce38f967eadf79f",
+    "url": "/testpage/static/js/runtime~main.e35f2ce3.js"
+  },
+  {
+    "revision": "a74fc1da6742b7fcd28cb01cde2c4566",
+    "url": "/testpage/static/media/vid.a74fc1da.png"
+  },
+  {
+    "revision": "3fbf6f56487c69c14d66738b16b6566e",
+    "url": "/testpage/static/media/search.3fbf6f56.svg"
+  },
+  {
+    "revision": "9988bef0264b9b786b082661fad3257f",
+    "url": "/testpage/static/media/profileIcon.9988bef0.PNG"
+  },
+  {
+    "revision": "2bfe10b6cb5be7d6e8444a44a2fef5ee",
+    "url": "/testpage/static/media/logo.2bfe10b6.PNG"
+  },
+  {
+    "revision": "3de5adbda30b3cc9a18a6c5e4644da52",
+    "url": "/testpage/static/media/thumbnail1.3de5adbd.png"
+  },
+  {
+    "revision": "86c330fafa5a11bb7cf91bf534d46b70",
+    "url": "/testpage/static/media/thumbnail2.86c330fa.png"
+  },
+  {
+    "revision": "ad8401390a6abb73daae",
+    "url": "/testpage/static/js/main.ad840139.chunk.js"
+  },
+  {
+    "revision": "0e80cd5bf32c0086f72115e7d28e9974",
+    "url": "/testpage/static/media/thumbnail4.0e80cd5b.png"
+  },
+  {
+    "revision": "d3458f6ac61d49ec7542c2f114931350",
+    "url": "/testpage/static/media/thumbnail5.d3458f6a.png"
+  },
+  {
+    "revision": "eb28ef37348456b38547ec52fd659df6",
+    "url": "/testpage/static/media/thumbnail6.eb28ef37.png"
+  },
+  {
+    "revision": "12d90df8958247b745c5931335806cfc",
+    "url": "/testpage/static/media/downloadTagPurple.12d90df8.svg"
+  },
+  {
+    "revision": "719508c948fa92acfdd1c96b741cc5c2",
+    "url": "/testpage/static/media/downloadTagBuild.719508c9.svg"
+  },
+  {
+    "revision": "0ad5a5a40db9207c10006b92cd5a7b47",
+    "url": "/testpage/static/media/fundable.0ad5a5a4.PNG"
+  },
+  {
+    "revision": "94688684eaef9037bc5bae7a8a3c9c62",
+    "url": "/testpage/static/media/heart.94688684.PNG"
+  },
+  {
+    "revision": "25b1b28097a8e4f3c964f36f11cd5c8e",
+    "url": "/testpage/static/media/messages.25b1b280.PNG"
+  },
+  {
+    "revision": "0f0e5f3a0e4e182bbaa62ab8cf31963d",
+    "url": "/testpage/static/media/select.0f0e5f3a.PNG"
+  },
+  {
+    "revision": "0450034ce6316b729649ac6a77f97b2f",
+    "url": "/testpage/index.html"
+  }
+];
